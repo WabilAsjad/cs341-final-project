4313510048a5b8f85b594326ffc2213443348e85
Wabil Asjad <wabilasjad@users.noreply.github.com>
2020-12-09 18:46:54 -0500

    add json

--- server.java
+++ server.java
@@ -4,8 +4,9 @@ import java.nio.file.*;
 import java.nio.charset.*;
 import java.util.*;
 import java.text.SimpleDateFormat;
+import org.json.simple.JSONObject;
 
-public class HTTPServer implements Runnable{
+public class HTTPServer implements Runnable {
 
     private Socket client;
     private InputStreamReader request;
@@ -24,7 +25,7 @@ public class HTTPServer implements Runnable{
      */
     private void parseRequest() {
         String input = "";
-        try{
+        try {
             String line = reader.readLine();
             // Parse input
             while (!line.isEmpty() || !line.equals("")) {
@@ -32,7 +33,7 @@ public class HTTPServer implements Runnable{
                 System.out.println(line);
                 line = reader.readLine();
             }
-        }catch(IOException ex){
+        } catch (IOException ex) {
             System.out.println("Exception on reading the line.");
         }
 
@@ -41,10 +42,14 @@ public class HTTPServer implements Runnable{
         String linkRequested = input.split("\n")[0].split(" ")[1].split("/")[1];
         System.out.println(linkRequested);
         System.out.println(input.split("\n")[0]);
-        if(input.split("\n")[0].contains("GET")) handleRequest("GET", linkRequested);
-        // else if(input.split("\n")[0].contains("POST")) handleRequest("POST", linkRequested, os);
-        else if(input.split("\n")[0].contains("PUT")) handleRequest("PUT", linkRequested);
-        else if(input.split("\n")[0].contains("DELETE")) handleRequest("DELETE", linkRequested);
+        if (input.split("\n")[0].contains("GET"))
+            handleRequest("GET", linkRequested);
+        // else if(input.split("\n")[0].contains("POST")) handleRequest("POST",
+        // linkRequested, os);
+        else if (input.split("\n")[0].contains("PUT"))
+            handleRequest("PUT", linkRequested);
+        else if (input.split("\n")[0].contains("DELETE"))
+            handleRequest("DELETE", linkRequested);
     }
 
     /**
@@ -54,17 +59,18 @@ public class HTTPServer implements Runnable{
      * @param String request
      * @return
      */
-    private void handleRequest(String method, String linkRequested){
-        try{
-            if (method.equals("GET")){
-                if(linkRequested.endsWith("/")) linkRequested += "index.html";
+    private void handleRequest(String method, String linkRequested) {
+        try {
+            if (method.equals("GET")) {
+                if (linkRequested.endsWith("/"))
+                    linkRequested += "index.html";
                 File file = new File(new File("."), linkRequested);
                 String contentType = getContentType(linkRequested);
-                int length = (int)file.length();
-                if(!file.exists() || file.isDirectory()){
+                int length = (int) file.length();
+                if (!file.exists() || file.isDirectory()) {
                     response(404);
                     os.flush();
-                }else{
+                } else {
                     byte[] fileBytes = Files.readAllBytes(Paths.get(linkRequested));
                     String fileString = new String(fileBytes, StandardCharsets.UTF_8);
                     response(200);
@@ -73,11 +79,11 @@ public class HTTPServer implements Runnable{
                     os.write(fileString);
                     os.flush();
                 }
-            }else if(method.equals("PUT")){
+            } else if (method.equals("PUT")) {
                 String input = "";
                 String line = reader.readLine();
-                while(line.contains("<html>")){
-                    while(!line.equals("</html>")){
+                while (line.contains("<html>")) {
+                    while (!line.equals("</html>")) {
                         line = reader.readLine();
                         input += line + "\n";
                         System.out.println(line);
@@ -85,51 +91,52 @@ public class HTTPServer implements Runnable{
                     input += "</html>";
                 }
 
-                if(input != ""){
+                if (input != "") {
                     int responseCode = writeData(input, linkRequested);
                     response(responseCode);
                     os.flush();
-                }else{
+                } else {
                     response(304);
                     os.flush();
                 }
-            }else if(method.equals("DELETE")){
+            } else if (method.equals("DELETE")) {
                 String deleteMessage = "<html>\n<body>\n<h1>File deleted.</h1>\n</body>\n</html>";
                 int responseCode = writeData(deleteMessage, linkRequested);
                 response(responseCode);
                 os.write(deleteMessage);
                 os.flush();
-            }else{
+            } else {
                 String error = "This is not a valid request.";
                 System.out.println(error);
             }
-        }catch (IOException e) {
+        } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
         // elif(request == "POST");
         // {
 
-        // }    
+        // }
     }
 
-    private void response(int responseCode){
-        if(responseCode == 200){
+    private void response(int responseCode) {
+        if (responseCode == 200) {
             os.write("HTTP/1.1 200 OK\r\n");
             os.write("Date: " + getTime() + "\r\n");
             os.write("Server: localhost\r\n");
-        }else if(responseCode == 404){
+        } else if (responseCode == 404) {
             // 404 page not found
             os.write("HTTP/1.1 404 Not Found\r\n");
             os.write("Date: " + getTime() + "\r\n");
             os.write("Server: localhost\r\n");
             os.write("\r\n");
-        }else if(responseCode == 304){
+        } else if (responseCode == 304) {
             os.write("HTTP/1.1 304 Not Modifies\r\n");
             os.write("Date: " + getTime() + "\r\n");
             os.write("Server: localhost\r\n");
             os.write("\r\n");
         }
     }
+
     /**
      * This function write data to the server.
      * 
@@ -137,18 +144,18 @@ public class HTTPServer implements Runnable{
      * @param linkRequested
      * @return
      */
-    private int writeData(String data, String linkRequested){
+    private int writeData(String data, String linkRequested) {
         File file = new File(new File("."), linkRequested);
-        if(!file.exists() || file.isDirectory()){
+        if (!file.exists() || file.isDirectory()) {
             return 404;
         }
         FileWriter filewriter;
-        try{
+        try {
             filewriter = new FileWriter(linkRequested);
             filewriter.write(data);
             filewriter.close();
             return 200; // Success
-        }catch(IOException ex){
+        } catch (IOException ex) {
             return 304;
         }
     }
@@ -159,9 +166,11 @@ public class HTTPServer implements Runnable{
      * @param String linkRequested
      * @return
      */
-    private String getContentType(String linkRequested){
-        if(linkRequested.endsWith("htm") || linkRequested.endsWith("html")) return "text/html";
-        else return "text/plain";
+    private String getContentType(String linkRequested) {
+        if (linkRequested.endsWith("htm") || linkRequested.endsWith("html"))
+            return "text/html";
+        else
+            return "text/plain";
     }
 
     /**
@@ -169,7 +178,7 @@ public class HTTPServer implements Runnable{
      * 
      * @return
      */
-    private String getTime(){
+    private String getTime() {
         Date date = new Date();
         SimpleDateFormat form = new SimpleDateFormat("MM/dd/yyyy h:mm:ss a, zzzz");
         return form.format(date);
@@ -190,33 +199,33 @@ public class HTTPServer implements Runnable{
                 Thread thread = new Thread(clientServer);
                 thread.start();
             }
-        }catch (IOException e) {
+        } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
     }
 
     @Override
-    public void run(){
+    public void run() {
         try {
             // Read HTTP request from the client socket
             request = new InputStreamReader(client.getInputStream());
             reader = new BufferedReader(request);
             os = new PrintWriter(client.getOutputStream());
-            
+
             // Parse Input
             parseRequest();
-            
+
             // Close the socket
             System.out.println("Closing the connection.");
-            if(reader != null){
+            if (reader != null) {
                 reader.close();
                 System.out.println("Socket input stream closed.");
             }
-            if(os != null){
+            if (os != null) {
                 os.close();
                 System.out.println("Socket output stream closed.");
             }
-            if(client != null){
+            if (client != null) {
                 client.close();
                 System.out.println("Socket closed.");
             }
