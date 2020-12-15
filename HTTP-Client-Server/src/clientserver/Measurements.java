25cd1a08e43bb63ec48f24977cc505cbedc3d2be
Annie <lannie9400@gmail.com>
2020-12-15 15:31:54 -0800

    README

--- HTTP-Client-Server/src/clientserver/Measurements.java
+++ HTTP-Client-Server/src/clientserver/Measurements.java
@@ -13,10 +13,18 @@ public class Measurements {
         requests = getList();
         Random rand = new Random();
 
-        int num = rand.nextInt(5);
+        int num = rand.nextInt(requests.size());
         command = requests.get(num).toString();
         System.out.println("Commands: " + command);
 
+        long startTime = System.nanoTime();
+        execution();
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime);
+        System.out.println("Timerrr: " + duration / 1000000000.0 + " seconds.");
+    }
+
+    public static void execution(){
         Runnable run = new Runnable() {
             @Override
             public void run() {
@@ -29,122 +37,118 @@ public class Measurements {
             }
         };
 
-        long startTime = System.nanoTime();
         pool = Executors.newFixedThreadPool(50);
         int count = 1;
-        while (count != 50) {
+        while (count != 10) {
             pool.execute(run);
             count++;
         }
         pool.shutdown();
-        long endTime = System.nanoTime();
-        long duration = (endTime - startTime);
-        System.out.println("Timer: " + duration / 1000000000.0 + " seconds.");
     }
 
     public static ArrayList getList() {
         requests = new ArrayList<String>();
         // GET method
         requests.add("GET database");
-        requests.add("GET Home.html");
-        requests.add("GET database/10");
-        requests.add("GET database/8/phoneNumbers");
-        requests.add("GET database/1/firstName");
-        requests.add("GET database/897/email");
-        requests.add("GET database/632/lastName");
-        requests.add("GET database/1000");
-        requests.add("GET database/284");
-        requests.add("GET database/382");
-        // Error checking
-        requests.add("GET databse");
-        requests.add("GET info.html");
-        requests.add("GET database/13022");
-        requests.add("GET database/742/age");
-        // HEAD method
-        requests.add("HEAD database");
-        requests.add("HEAD database/492");
-        requests.add("HEAD database/592/firstName");
-        requests.add("HEAD database/793/lastName");
-        requests.add("HEAD database/465/gender");
-        requests.add("HEAD database/952/phoneNumbers");
-        requests.add("HEAD Home.html");
-        requests.add("HEAD database/820");
-        requests.add("HEAD database/472");
-        requests.add("HEAD database/624");
-        // Error checking
-        requests.add("HEAD datase");
-        requests.add("HEAD info.html");
-        requests.add("HEAD database/1842");
-        requests.add("HEAD database/274/age");
-        // PUT method
-        requests.add("PUT database/385/firstName/mickey");
-        requests.add("PUT database/824/firstName/Lily");
-        requests.add("PUT database/248/lastName/Bon");
-        requests.add("PUT database/932/lastName/Kim");
-        requests.add("PUT database/444/email/helloworld@gmail.com");
-        requests.add("PUT database/777/email/hello@yahoo.com");
-        requests.add("PUT database/946/gender/Female");
-        requests.add("PUT database/673/gender/Male");
-        requests.add("PUT database/382/phoneNumbers/7777777777");
-        requests.add("PUT database/724/phoneNumbers/123456789");
-        // Error checking
-        requests.add("PUT database/284/lastName");
-        requests.add("PUT database");
-        requests.add("PUT database/93");
-        requests.add("PUT databse");
-        // POST method
-        requests.add("POST database/Kellie/Shemelt/Female/kshemelt0@cocolog-nifty.com/4318353287");
-        requests.add("POST database/Abbot/Scantleberry/Male/ascantleberry1@usgs.gov/1604841661");
-        requests.add("POST database/Con/Rosier/Male/crosier2@amazon.co.uk/4153128592");
+        // requests.add("GET Home.html");
+        // requests.add("GET database/10");
+        // requests.add("GET database/8/phoneNumbers");
+        // requests.add("GET database/1/firstName");
+        // requests.add("GET database/897/email");
+        // requests.add("GET database/632/lastName");
+        // requests.add("GET database/1000");
+        // requests.add("GET database/284");
+        // requests.add("GET database/382");
+        // // Error checking
+        // requests.add("GET databse");
+        // requests.add("GET info.html");
+        // requests.add("GET database/13022");
+        // requests.add("GET database/742/age");
+        // // HEAD method
+        // requests.add("HEAD database");
+        // requests.add("HEAD database/492");
+        // requests.add("HEAD database/592/firstName");
+        // requests.add("HEAD database/793/lastName");
+        // requests.add("HEAD database/465/gender");
+        // requests.add("HEAD database/952/phoneNumbers");
+        // requests.add("HEAD Home.html");
+        // requests.add("HEAD database/820");
+        // requests.add("HEAD database/472");
+        // requests.add("HEAD database/624");
+        // // Error checking
+        // requests.add("HEAD datase");
+        // requests.add("HEAD info.html");
+        // requests.add("HEAD database/1842");
+        // requests.add("HEAD database/274/age");
+        // // PUT method
+        // requests.add("PUT database/385/firstName/mickey");
+        // requests.add("PUT database/824/firstName/Lily");
+        // requests.add("PUT database/248/lastName/Bon");
+        // requests.add("PUT database/932/lastName/Kim");
+        // requests.add("PUT database/444/email/helloworld@gmail.com");
+        // requests.add("PUT database/777/email/hello@yahoo.com");
+        // requests.add("PUT database/946/gender/Female");
+        // requests.add("PUT database/673/gender/Male");
+        // requests.add("PUT database/382/phoneNumbers/7777777777");
+        // requests.add("PUT database/724/phoneNumbers/123456789");
+        // // Error checking
+        // requests.add("PUT database/284/lastName");
+        // requests.add("PUT database");
+        // requests.add("PUT database/93");
+        // requests.add("PUT databse");
+        // // POST method
+        // requests.add("POST database/Kellie/Shemelt/Female/kshemelt0@cocolog-nifty.com/4318353287");
+        // requests.add("POST database/Abbot/Scantleberry/Male/ascantleberry1@usgs.gov/1604841661");
+        // requests.add("POST database/Con/Rosier/Male/crosier2@amazon.co.uk/4153128592");
 
-        requests.add("PUT database/1/gender/female");
-        requests.add("GET database/212");
-        requests.add("PUT database/20/email/gtongj@school.edu");
-        requests.add("GET database/952");
-        requests.add("PUT database/520/firstName/wabil");
-        requests.add("HEAD database");
+        // requests.add("PUT database/1/gender/female");
+        // requests.add("GET database/212");
+        // requests.add("PUT database/20/email/gtongj@school.edu");
+        // requests.add("GET database/952");
+        // requests.add("PUT database/520/firstName/wabil");
+        // requests.add("HEAD database");
 
-        requests.add("GET database/912");
-        requests.add("PUT database/993/lastName/Geos");
-        requests.add("GET database/753");
-        requests.add("DELETE database/3");
-        requests.add("DELETE database/999/lastName/deleting");
+        // requests.add("GET database/912");
+        // requests.add("PUT database/993/lastName/Geos");
+        // requests.add("GET database/753");
+        // requests.add("DELETE database/3");
+        // requests.add("DELETE database/999/lastName/deleting");
 
-        requests.add("GET database/628");
-        requests.add("DELETE database/999");
-        requests.add("POST database/Mary/Scan/Female/mscan@mit.gov/1836841699");
-        requests.add("POST database/Moira/Rose/Female/mrose@creek.gov/1604729357");
-        requests.add("POST database/Alexis/Rose/Female/arose1@creek.gov/1678341865");
+        // requests.add("GET database/628");
+        // requests.add("DELETE database/999");
+        // requests.add("POST database/Mary/Scan/Female/mscan@mit.gov/1836841699");
+        // requests.add("POST database/Moira/Rose/Female/mrose@creek.gov/1604729357");
+        // requests.add("POST database/Alexis/Rose/Female/arose1@creek.gov/1678341865");
 
-        requests.add("POST database/Abbotttt/Scantleberryyyy/Female/ascantleberry123@usgs.gov/1604841661");
-        requests.add("GET database/617");
-        requests.add("DELETE database/82");
-        requests.add("DELETE database/72");
-        requests.add("PUT database/791/firstName/wabil");
+        // requests.add("POST database/Abbotttt/Scantleberryyyy/Female/ascantleberry123@usgs.gov/1604841661");
+        // requests.add("GET database/617");
+        // requests.add("DELETE database/82");
+        // requests.add("DELETE database/72");
+        // requests.add("PUT database/791/firstName/wabil");
 
-        requests.add("POST database/Bag/Sweater/Male/bsweater13@wellesley.gov/1604841999");
-        requests.add("GET database/1002");
-        requests.add("DELETE database/327");
-        requests.add("PUT database/759/firstName/annie");
-        requests.add("HEAD database");
+        // requests.add("POST database/Bag/Sweater/Male/bsweater13@wellesley.gov/1604841999");
+        // requests.add("GET database/1002");
+        // requests.add("DELETE database/327");
+        // requests.add("PUT database/759/firstName/annie");
+        // requests.add("HEAD database");
 
-        requests.add("DELETE database/351");
-        requests.add("PUT database/247/firstName/annie");
-        requests.add("GET database/1999"); // Error check
-        requests.add("GET database/459");
-        requests.add("GET database/289");
+        // requests.add("DELETE database/351");
+        // requests.add("PUT database/247/firstName/annie");
+        // requests.add("GET database/1999"); // Error check
+        // requests.add("GET database/459");
+        // requests.add("GET database/289");
 
-        requests.add("PUT database/481/gender/Male");
-        requests.add("PUT database/887/lastName/testing");
-        requests.add("PUT database/853/gender/Female");
-        requests.add("PUT database/422/gender/Female");
-        requests.add("PUT database/333/gender/Female");
+        // requests.add("PUT database/481/gender/Male");
+        // requests.add("PUT database/887/lastName/testing");
+        // requests.add("PUT database/853/gender/Female");
+        // requests.add("PUT database/422/gender/Female");
+        // requests.add("PUT database/333/gender/Female");
 
-        requests.add("POST database/Jerry/Tom/Female/jtom1@usgs.gov/1604841999");
-        requests.add("GET database/241");
-        requests.add("PUT database/667/email/fdgjkdfhg@email.edu");
-        requests.add("PUT database/622/firstName/testing");
-        requests.add("POST database/David/Rose/Male/drose@creek.gov/1604782636");
+        // requests.add("POST database/Jerry/Tom/Female/jtom1@usgs.gov/1604841999");
+        // requests.add("GET database/241");
+        // requests.add("PUT database/667/email/fdgjkdfhg@email.edu");
+        // requests.add("PUT database/622/firstName/testing");
+        // requests.add("POST database/David/Rose/Male/drose@creek.gov/1604782636");
         return requests;
     }
 }
@@ -164,3 +168,24 @@ public class Measurements {
 // making a very large request.
 // Or a client making very small requests but more frequently.
 // As the clients grow, does latency increase?
+
+// Mesurements: 
+// 10 clients; 50 clients; 100 clients
+// For GET request
+// 1. GET database vs. GET mini_database
+// 2. GET database/1 vs. GET mini_database/1
+// 3. GET database/1/firstName vs. GET mini_database/1/firstName
+// 3. GET database/1000/phoneNumbers vs. GET mini_database/20/phoneNumbers
+// For POST request
+// 1. POST database/Jerry/Tom/Female/jtom1@usgs.gov/1604841999 vs. POST mini_database/Jerry/Tom/Female/jtom1@usgs.gov/1604841999
+// FOR PUT request
+// 1. PUT database/1/firstName/annie vs. PUT mini_database/1/firstName/annie
+// 2. PUT database/1000/firstName/annie vs. PUT mini_database/20/firstName/annie
+// FOR DELETE request
+// 1. DELETE database vs. DELETE mini_database
+// 2. DELETE database/1 vs. DELETE mini_database/1
+// 3. DELETE database/1/firstName vs. DELETE mini_database/1/firstName
+// 3. DELETE database/1000/phoneNumbers vs. DELETE mini_database/20/phoneNumbers
+
+// 1 clients only making GET request 
+// 1 clients only making POST request
--- README.md
+++ README.md
@@ -1,28 +1,90 @@
-## HTTP Client-Server
-* Wellesley College CS341 Operating Systems - Final Project
-* Authors: Wabil Asjad & Yaxin Liu
+## Multithreaded HTTP Server 
+- Wellesley College CS341 Operating Systems - Final Project
+- Authors: Wabil Asjad & Yaxin Liu
+
+## Description
+This is a multithreading HTTP server based on socket TCP in java. The locally hosted server is a database server that provides http/rest interface. Requests include GET, HEAD, POST, PUT, and DELETE.
 
 ## Folder Structure
 
 The workspace contains two folders by default, where:
 
 - `src`: the folder to maintain sources
+    - `clientserver`: the folder to maintain files for measurement and to run the client server
 - `lib`: the folder to maintain dependencies
+
+## Description of Files
+
+- `Client.java`: a client that sends HTTP requests to the server
+- `httpServer.java`: a locally hosted server that listens for requests, handles requests, and responses to client
+- `formatJSON.java`: consists of code to format databases we downloaded from online
+- `Measurements.java`: consists of code to measure the effectiveness of the server
+- `database.json`: a database with 1000 data
+- `Home.html`: a simple HTML test file for GET and DELETE request
+
+## Features
+Our multithreaded HTTP server provides HTTP/REST interface. We implemented the following requests:
+- `GET`: read the data, retrieve it, and return that to the client
+    - `GET` data from database:
+        - `GET` whole database: `GET database`
+        - `GET` a JSONObject from database: `GET database/1`
+        - `GET` a key from a JSONObject: `GET database/1/email`
+    - `GET` data from HTML: `GET Home.html`
+- `HEAD`: read the data
+    - `HEAD` data from database:
+        - `HEAD` whole database: `HEAD database`
+        - `HEAD` a JSONObject from database: `HEAD database/1`
+        - `HEAD` a key from a JSONObject: `HEAD database/1/email`
+    - `HEAD` data from HTML: `HEAD Home.html`
+- `DELETE`: delete all the data from the target location requested by the client
+    - `DELETE` data from database: 
+        - `DELETE` whole database: `DELETE database`
+        - `DELETE` a JSONObject from database: `DELETE database/1`
+        - `DELETE` a key from a JSONObject: `DELETE database/1/email`
+    - `DELETE` data from HTML: `DELETE Home.html`
+- `POST`: create or add a new item to the database
+    - ` POST` data to the database: `POST database/annie/liu/Female/12345@gmail.com/123456789`
+- `PUT`: modify or replace the current data with the requested data
+    - `PUT` data to the database: `PUT database/1/firstName/annie`
+
 
 ## Dependency Management
 
-We imported two jar files in `lib` folder: `gson-2.7.jar` and `json-simple-1.1.1.jar`. Before running the program, 
+We imported two jar files in `lib` folder: 
+- `gson-2.7.jar`: provides helper functions and common JSON structures using GSON.
+- `json-simple-1.1.1.jar`: deserialize and serialize JSON
 
-Instructions to install required software dependencies (with links to software or other documentation as needed) and compile and configure your system.
+
+## Setting Up the Envinronment
+- In order to run Java in Visual Studio Code, please follow the link and install the coding pack and the Java extension pack: https://code.visualstudio.com/docs/languages/java#_install-visual-studio-code-for-java
+- After opening Visual Studio Code, click `File` on the top left, then click `Open` to open the entire `HTTP-Client-Server` folder. 
+
+![](https://i.imgur.com/jjaeEd7.gif)
+
+- Once opened, go to `Java Project` and in `Referecence`, check the existence of these two jar files. If not, click `+`, browse to `HTTP-Client-Server/lib/`, and manually add the jar files.
+
+![](https://i.imgur.com/3Al9qJ1.gif)
+
+- Then opened `setting.json` file in `.vscode`, check if you see the below information
+
+![](https://i.imgur.com/wFWavY4.png)
+
+- Now, you are ready to run the server. 
 
 ## Running HTTP Client-Server
-* In order to run Java in Visual Studio Code, please follow the link and install the coding pack and the Java extension pack: https://code.visualstudio.com/docs/languages/java#_install-visual-studio-code-for-java
-* After opening Visual Studio Code, click `File` then `Open`, then open the entire folder, HTTP-Client-Server folder. 
+- Right click `httpSever.java`, then click `Run`. You should see 
+`Listening for connection on port 8082 ....` 
+in the terminal if the server runs successfully. 
+- Right click `Client.java`, then click `Run`. You should see
+`Starting client server...`
+`Enter your request:` 
+in the terminal if the client runs successfully.
 
-## Features
+![](https://i.imgur.com/uuurh88.gif)
 
-## Description of Files
-* 
+
 
 ## Assumptions and Limitations
-Instructions to run your system on sample or test inputs and interpret its outputs. These instructions/suggested tests should demonstrate that it works. If compiling or running your system is tricky or depends on a specific environment, it’s best to include transcripts of the output of each of these suggested tests.
+
+## Learn More
+Check out the slide: https://docs.google.com/presentation/d/195akDiXrJWZK4GMb9WR8_6hf7aldFTZeucLE-SQr5JI/edit?usp=sharing
